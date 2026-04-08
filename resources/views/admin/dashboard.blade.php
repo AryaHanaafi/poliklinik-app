3721110e07c54a690267d21ac2b49ce73bef8f39
AryaHanaafi <aryahanaafi115@gmail.com>
2026-04-08 19:05:51 +0700

    Menyelesaikan Tugas 2 Modul 4-6

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app title="Admin Dashboard">
+    <h1 class="ml-4"> Halo Selamat datang Admin</h1>
+</x-layouts.app>
---
+++ resources/views/pasien/dashboard.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app title="Pasien Dashboard">
+    <h1 class="ml-4"> Halo Selamat datang Pasien</h1>
+</x-layouts.app>
